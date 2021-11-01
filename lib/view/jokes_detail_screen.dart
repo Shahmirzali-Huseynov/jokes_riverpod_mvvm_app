e978e6ee024e9935bd0b264370a77386b9cc59af
sahmirzeli <sahmirzeli.huseynov@gmail.com>
2021-11-01 15:05:38 +0300

    second commit

--- lib/view/jokes_detail_screen.dart
+++ lib/view/jokes_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chucknorrisjokes/core/utils/colors.dart';
 import 'package:chucknorrisjokes/core/utils/custom_textstyle.dart';
 import 'package:chucknorrisjokes/core/widgets/button_widget.dart';
+import 'package:chucknorrisjokes/view/error_view/error_screen.dart';
+import 'package:chucknorrisjokes/view/error_view/internet_error_screen.dart';
 import 'package:chucknorrisjokes/view_model/jokes_detail_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -22,49 +24,53 @@ class JokesDetailScreen extends StatelessWidget {
       ),
       body: Consumer(builder: (context, watch, child) {
         final jokesDetail = watch(jokesDetailViewModelProvider);
-        return jokesDetail.getLoadingStaus != JokesDetailStatus.COMPLETED
-            ? const Center(
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.white,
-                ),
-              )
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Card(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: context.screenWidth * 0.05,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: context.screenWidth * 0.05,
-                        vertical: context.screenWidth * 0.05,
-                      ),
-                      child: Text(
-                        "${jokesDetail.getJokesDetail?.value}",
-                        style: CustomTextStyles(context).headline1(
-                          fontColor: ColorConst().blackColor,
-                          fontWeight: FontWeight.w400,
+        return jokesDetail.getLoadingStaus == JokesDetailStatus.NOT_INTERNET_CONNECTION
+            ? const InternetErrorScreen()
+            : jokesDetail.getLoadingStaus == JokesDetailStatus.ERROR
+                ? const ErrorPage()
+                : jokesDetail.getLoadingStaus != JokesDetailStatus.COMPLETED
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.white,
                         ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: context.screenHeight * 0.02,
-                  ),
-                  ButtonWidget(
-                    text: 'Copy',
-                    onClicked: () {
-                      Clipboard.setData(
-                        ClipboardData(
-                          text: jokesDetail.getJokesDetail?.value,
-                        ),
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Card(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: context.screenWidth * 0.05,
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: context.screenWidth * 0.05,
+                                vertical: context.screenWidth * 0.05,
+                              ),
+                              child: Text(
+                                "${jokesDetail.getJokesDetail?.value}",
+                                style: CustomTextStyles(context).headline1(
+                                  fontColor: ColorConst().blackColor,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: context.screenHeight * 0.02,
+                          ),
+                          ButtonWidget(
+                            text: 'Copy',
+                            onClicked: () {
+                              Clipboard.setData(
+                                ClipboardData(
+                                  text: jokesDetail.getJokesDetail?.value,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       );
-                    },
-                  ),
-                ],
-              );
       }),
     );
   }
